6c5c62151a52efd48d9750af8ccf4f96c42540d5
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2014-08-27 17:31:35 -0700

    adding all my shit

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = function (grunt) {
+  var config = {
+    jshint: {
+      options: {
+        ignores: ['node_modules/**', 'public/vendor/**', '**/*.min.js'],
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: 'Gruntfile.js',
+      server: ['routes/**/*.js', 'app.js', 'config.js'],
+      client: 'public/**/*.js'
+    },
+    stylus: {
+      compile: {
+        options: {
+          use: [require('jeet'),require('nib')]
+        },
+        files: {
+          'public/css/styles.css': ['public/stylus/*.styl']
+        }
+      }
+      
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'public/css',
+        src: ['*.css', '!*.min.css'],
+        dest: 'public/css',
+        ext: '.min.css'
+      },
+      addBanner: {
+        options: {
+          banner: '/*!\n* DevCult MotherFUCKERS\n* http://www.devcult.com/\n*\n* Copyright 2014, 2666 CULT Foundation, Inc. \n*/'
+        },
+        files: {
+          'public/css/styles.css': ['public/css/styles.min.css']
+        }
+      }
+    },
+    'node-inspector': {
+      options: {
+        'save-live-edit': true
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'app.js',
+        options: {
+          nodeArgs: ['--debug'],
+          cwd: __dirname,
+          ignore: ['node_modules/', 'public/'],
+          ext: 'js',
+          watch: ['routes/**/*.js', 'app.js', 'config.js'],
+          delay: 1,
+          legacyWatch: true
+        }
+      }
+    },
+    watch: {
+      all: {
+        files: ['public/**/*', 'views/**', '!**/node_modules/**', '!public/vendor/**/*', '!**/*.min.*'],
+        options: {
+          livereload: 3006
+        }
+      },
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: 'jshint:gruntfile'
+      },
+      scripts: {
+        files: 'public/js/**/*.js',
+        tasks: 'jshint:client'
+      },
+      server: {
+        files: ['routes/**/*.js', 'app.js', 'config.js'],
+        tasks: 'jshint:server'
+      },
+      styl: {
+        files: ['public/stylus/**/*.styl'],
+        tasks: ['stylus', 'cssmin']
+      }
+    },
+    concurrent: {
+      tasks: ['nodemon', 'node-inspector', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
+    }
+  };
+
+  grunt.initConfig(config);
+
+  // Load the tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint', 'stylus', 'cssmin', 'concurrent']);
+};
---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+  development: {
+    server: {
+      port: 3000
+    },
+    htmlPretty: true
+  },
+  testing: {
+    server: {
+      port: 3001
+    }
+  },
+  production: {
+    server: {
+      port: 8080
+    }
+  }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];
